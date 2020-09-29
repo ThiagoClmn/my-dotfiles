69652a898ad2face66d3365efbea50abc81b8699
Thiago Calomino <38413362+ThiagoClmn@users.noreply.github.com>
2020-09-29 17:55:49 -0300

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 # make sure you run this script as root so you don't sudo all the time
-# my current is an Ubuntu 20.04
+# my current OS is an Ubuntu 20.04
 apt update
 apt upgrade -y
 
 apt install git octave default-jdk build-essential gcc g++ gfortran -y
 apt install vim gimp gparted htop gnome-tweaks zsh silversearcher-ag -y
 
-# Docker#################
+# Docker################# see if it needs some update
 # get docker public key
 sudo apt-get install \
     apt-transport-https \
@@ -34,6 +34,8 @@ git clone https://github.com/daniruiz/flat-remix-gtk
 mkdir -p ~/.icons && mkdir -p ~/.themes
 cp -r flat-remix/Flat-Remix* ~/.icons/ && cp -r flat-remix-gtk/Flat-Remix-GTK* ~/.themes/
 
+
+# some other tools to add such as...
 # .Net Core platform
 # get microsoft keys
 # sdk
